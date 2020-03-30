f85e675e96e66fec2003e9574dcadea4639b2ddb
R2H <R2H@administrators-MacBook-Pro.local>
2020-03-30 11:14:19 -0400

    checkpoint1

---
+++ checkpoint-arrays/style.js
@@ -0,0 +1,11 @@
+// array methods
+//  .push, .pop, .shift
+
+
+
+var drinks = new Array (' water', 'coffee', 'tea ')
+var foods = new Array('apple', 'sandwich', 'chips')
+var meals = new Array(drinks + foods)
+console.log(meals)
+
+function reverseArray 
