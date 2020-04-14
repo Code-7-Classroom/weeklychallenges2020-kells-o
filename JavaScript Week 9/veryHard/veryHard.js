3724de97c715d6efff4f78084880dec85c20854f
R2H <R2H@administrators-MacBook-Pro.local>
2020-04-13 21:08:23 -0400

    Week 9 Javascript Challenges

---
+++ JavaScript Week 9/veryHard/veryHard.js
@@ -0,0 +1,13 @@
+function coinCombos(coinArr, total){
+for (var i = total; i > 0; i){
+
+}
+}
+
+coinCombos([1,5, 10, 25], 90)
+
+//create a loop. 
+//decide most efficient way to give coins back with a condition
+//if you can't give back highest coin check for the second largest
+//if i cant take 25 from 90 check how many times to give 10 and so on
+//if theres a remainder and no way to give coins back return -1
