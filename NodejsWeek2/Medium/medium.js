f0ed2bece4cc07227a9056c32973970dc4b6b592
R2H <R2H@administrators-MacBook-Pro.local>
2020-05-05 21:30:31 -0400

    Node challenges

---
+++ NodejsWeek2/Medium/medium.js
@@ -0,0 +1,36 @@
+// MEDIUM: Create a JSON file that will have 10 employees in it, their employeeID, 
+// their name, their salary and department name.
+// Then, create an express API so that when you hit the endpoint with a GET request we 
+// want the api to respond with all data on the employees.
+// If you hit the endpoint with their employeeID, we want to hand up only the information on that one employee.
+// If you hit the endpoint with an incorrect employeeID, send back the correct HTTP status 
+// code and an error message stating that the employee was not found.
+// GET::myendpointname.com/employees = Json with information from all 10 employees.
+// GET::myendpointname.com/employees/<employeeID> = Json with the information from that specific employee.
+
+
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000
+const employees = require('./medium.json')
+
+
+app.get('/medium', (req, res) => {
+    res.send(employees)
+  });
+
+  app.get('/medium/:id', (req, res) => {
+      employees.find(e => e.id === parseInt(req.params.id));
+    res.send(employees.id)
+
+    if (!employees) {
+        return res.status(404).send("Employee not found.")
+    }
+  });
+
+
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+
